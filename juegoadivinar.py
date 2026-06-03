e63012cabaf0fed8a33f68a470120950a789c2c8
Hesiquio Zarate Landa <zarate@itsescarcega.edu.mx>
2026-06-03 11:58:53 -0600

    Update juegoadivinar.py

--- juegoadivinar.py
+++ juegoadivinar.py
@@ -1,3 +1,6 @@
 print("Bienvenido al juego de adivinar el número.")
 print("Estoy pensando en un número entre 1 y 100.")
 print("Tienes 10 intentos para adivinar el número.")
+import random
+numero_secreto = random.randint(1, 100)
+intentos = 10
